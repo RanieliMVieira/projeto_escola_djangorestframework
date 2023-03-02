9005611e91a7f9d91597aaa24bb569a51704ec58
ranielivieira <ranily_vieira@hotmail.com>
2023-03-02 15:01:29 -0300

    Adicionado relações e paginação DRF

--- cursos/models.py
+++ cursos/models.py
@@ -17,14 +17,14 @@ class Curso(Base):
     class Meta:
         verbose_name = 'Curso'
         verbose_name_plural = 'Cursos'
+        ordering = ['id']
 
     def __str__(self) -> str:
         return self.titulo
 
 
 class Avaliacao(Base):
-    curso = models.ForeignKey(
-        Curso, related_name='avaliacoes', on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, related_name='avaliacoes', on_delete=models.CASCADE)
     nome = models.CharField(max_length=255)
     email = models.EmailField()
     comentario = models.TextField(blank=True, default='')
@@ -34,6 +34,7 @@ class Avaliacao(Base):
         verbose_name = 'Avaliação'
         verbose_name_plural = 'Avaliações'
         unique_together = ['email', 'curso']
+        ordering = ['id']
 
     def __str__(self) -> str:
         return f'{self.nome} avaliou o curso {self.curso} com nota {self.avaliacao}'  # noqa: E501
--- cursos/serializers.py
+++ cursos/serializers.py
@@ -23,13 +23,21 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
 
 
 class CursoSerializer(serializers.ModelSerializer):
+    # Nested Relationship
+    # avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
 
+    #HyperLinked Related Field
+    #avaliacoes = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='avaliacao-detail')
+
+    # Primar Key Related Field
+    avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     class Meta:
-        model = Curso
+        model = Curso 
         fields = (
             'id',
             'titulo',
             'url',
             'criacao',
             'ativo',
+            'avaliacoes',
         )
